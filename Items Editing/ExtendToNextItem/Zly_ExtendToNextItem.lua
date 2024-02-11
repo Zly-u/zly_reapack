1a27315089ecfbe46e8a0458268e439fa1fc1346
Zly <zlyudchik@gmail.com>
2024-02-11 18:08:52 +0300

    - fucking

--- Items Editing/ExtendToNextItem/Zly_ExtendToNextItem.lua
+++ Items Editing/ExtendToNextItem/Zly_ExtendToNextItem.lua
@@ -1,16 +1,6 @@
 --[[
-@description Extend To Next Item
-@author Zly
-@version 1.0
-@provides
-	[main] .
-@about
-	# Extend To Next Item
-
-	- Extends selected Items to next adjacent item.
-@changelog
-	- Init
---]]
+@noindex
+]]--
 
 
 function main()
---
+++ Items Editing/meta_ExtendToNextItem.lua
@@ -0,0 +1,14 @@
+--[[
+@metapackage
+@description Extend To Next Item
+@author Zly
+@version 1.0
+@provides
+	[main] ExtendToNextItem/Zly_ExtendToNextItem.lua
+@about
+	# Extend To Next Item
+
+	- Extends selected Items to next adjacent item.
+@changelog
+	- Init
+--]]
