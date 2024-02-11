740b8aae0b669d168d0ec3e6896eb423c5de1364
Zly <zlyudchik@gmail.com>
2024-02-11 04:53:29 +0300

    - removed meta

--- VideoAutoFlipper/Zly_VideoAutoFlipper.lua
+++ VideoAutoFlipper/Zly_VideoAutoFlipper.lua
@@ -1,5 +1,17 @@
--- @noindex
-
+--[[
+@description Video Auto-Flipper
+@author Zly
+@version 1.0
+@provides
+   [main] .
+   [nomain] VAF_GUI.lua
+   [nomain] VAF_CORE.lua
+@about
+   # Video Auto-Flipper
+   
+   - Flips selected items.
+   - Has a handy GUI and other featues for ease of simple YTPMV making.
+--]]
 
 local core  = require("VAF_CORE")
 local gui   = require("VAF_GUI")
