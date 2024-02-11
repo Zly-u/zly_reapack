55e8b955d7e0c29171ac22c109aa9ec0cb5b0b21
Zly <zlyudchik@gmail.com>
2024-02-11 18:11:46 +0300

    - fucking fix for this

---
+++ Items Editing/meta_StretchToNextItem.lua
@@ -0,0 +1,14 @@
+--[[
+@metapackage
+@description Stretch To Next Item
+@author Zly
+@version 1.0
+@provides
+	[main] StretchToNextItem/Zly_StretchToNextItem.lua
+@about
+	# Stretch To Next Item
+
+	- Stretches selected Items to next adjacent item.
+@changelog
+	- Init
+--]]
