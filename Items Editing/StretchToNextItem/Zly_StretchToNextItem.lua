3e3ee48194c8abc6056650cdb14f6d1be5ee9e8a
Zly <zlyudchik@gmail.com>
2024-02-11 18:12:08 +0300

    forgot FUCKING @noindex

--- Items Editing/StretchToNextItem/Zly_StretchToNextItem.lua
+++ Items Editing/StretchToNextItem/Zly_StretchToNextItem.lua
@@ -1,15 +1,5 @@
 --[[
-@description Stretch To Next Item
-@author Zly
-@version 1.0
-@provides
-	[main] .
-@about
-	# Stretch To Next Item
-
-	- Stretches selected Items to next adjacent item.
-@changelog
-	- Init
+@noindex
 --]]
 
 
