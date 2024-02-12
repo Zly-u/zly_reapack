90b66627147dce585ba1426a2f56e089d6bab5f9
Zly <zlyudchik@gmail.com>
2024-02-13 01:11:51 +0300

    Updated metadata

--- MIDI/meta_ItemsBasedOnMIDI.lua
+++ MIDI/meta_ItemsBasedOnMIDI.lua
@@ -2,7 +2,7 @@
 @metapackage
 @description Items Based On MIDI
 @author Zly
-@version 1.1
+@version 1.1.1
 @provides
 	[main] .\ItemsBasedOnMIDI\Zly_ItemsBasedOnMIDI.lua
 @about
@@ -12,5 +12,6 @@
 	- Each of the item's pitch is based off the first note in the MIDI.
 	- Inherits Volume from midi as well.
 @changelog
-	- Added proper support for Chords and overlapping by time notes
+	- Reversed tracks to more or less represent the midi.
+	- Made it so all the notes refer the very first note in the track
 --]]
