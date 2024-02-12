13278f9554acf2ced28581cf754f128fba5bc6c2
Zly <zlyudchik@gmail.com>
2024-02-12 15:50:29 +0300

    - Working code for ItemsBasedOnMIDI

--- MIDI/ItemsBasedOnMIDI/Zly_ItemsBasedOnMIDI.lua
+++ MIDI/ItemsBasedOnMIDI/Zly_ItemsBasedOnMIDI.lua
@@ -2,53 +2,58 @@
 
 function main()
 	local item_count = reaper.CountSelectedMediaItems()
-	if item_count == 0 then return end
+	if item_count == 0 then
+		reaper.MB("No MIDI was selected", "Error", 0)
+		return
+	end
 
 	reaper.Undo_BeginBlock()
 
 	-- Just trying to make a Track adjacent to our original track of selected items
-	local first_item	= reaper.GetSelectedMediaItem(0, 0)
-	local track			= reaper.GetMediaItemTrack(first_item)
-	local _, track_name = reaper.GetTrackName(track)
-	local track_index	= reaper.GetMediaTrackInfo_Value(track, "IP_TRACKNUMBER")
-	reaper.InsertTrackAtIndex(track_index-1, true)
-	local new_midi_track = reaper.GetTrack(0, track_index-1)
-	reaper.GetSetMediaTrackInfo_String(new_midi_track, "P_NAME", track_name..(track_name ~= "" and "_" or "").."MIDI", true)
+	local midi_item	= reaper.GetSelectedMediaItem(0, 0)
+	local midi_take = reaper.GetActiveTake(midi_item)
 
-	-- Create MIDI item
-	local first_item_start		= reaper.GetMediaItemInfo_Value(first_item, "D_POSITION")
-	local last_item				= reaper.GetSelectedMediaItem(0, item_count - 1)
-	local last_item_start		= reaper.GetMediaItemInfo_Value(last_item, "D_POSITION")
-	local last_item_duration	= reaper.GetMediaItemInfo_Value(last_item, "D_LENGTH")
-
-	local MIDI_startPos	= first_item_start
-	local MIDI_endPos	= last_item_start+last_item_duration
-	local midi_item		= reaper.CreateNewMIDIItemInProj(new_midi_track, MIDI_startPos, MIDI_endPos, false)
-	local midi_take		= reaper.GetActiveTake(midi_item)
-
-
-	for id = 0, item_count - 1 do
-		local item			= reaper.GetSelectedMediaItem(0, id)
-		local item_start	= reaper.GetMediaItemInfo_Value(item, "D_POSITION")
-		local item_duration	= reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
-
-		local take			= reaper.GetActiveTake(item)
-		local take_pitch	= reaper.GetMediaItemTakeInfo_Value(take, "D_PITCH")+63
-		local take_volume	= reaper.GetMediaItemTakeInfo_Value(take, "D_VOL")*127
-		take_pitch = math.min(math.max(take_pitch, 0), 127)
-
-		reaper.MIDI_InsertNote(
-			midi_take, false, false, 												-- MediaItem_Take take, boolean selected, boolean muted,
-			reaper.MIDI_GetPPQPosFromProjTime(midi_take, item_start),				-- number startppqpos,
-			reaper.MIDI_GetPPQPosFromProjTime(midi_take, item_start+item_duration),	-- number endppqpos,
-			0, take_pitch, take_volume,												-- integer chan, integer pitch, integer vel
-			false																	-- optional boolean noSortIn
-		)
-
+	if not reaper.TakeIsMIDI(midi_take) then
+		reaper.MB("The selected Item is not a MIDI", "Error", 0)
+		return
 	end
-	reaper.MIDI_Sort(midi_take)
 
-	reaper.Undo_EndBlock("MIDI Based on Items", 0)
+	local midi_track	= reaper.GetMediaItemTrack(midi_item)
+	local _, track_name = reaper.GetTrackName(midi_track)
+	local track_index	= reaper.GetMediaTrackInfo_Value(midi_track, "IP_TRACKNUMBER")
+	reaper.InsertTrackAtIndex(track_index - 1, true)
+	local new_items_track = reaper.GetTrack(0, track_index - 1)
+	reaper.GetSetMediaTrackInfo_String(new_items_track, "P_NAME", track_name..(track_name ~= "" and "_" or "").."ITEMS", true)
+
+	local _, notes_num, _, _ = reaper.MIDI_CountEvts(midi_take)
+	local first_base_pitch = 0
+	for id = 0, notes_num - 1 do
+		local _, _, muted, start_ppq_pos, end_ppq_pos, _, pitch, vel = reaper.MIDI_GetNote(midi_take, id)
+		if muted then
+			goto continue
+		end
+
+		if id == 0 then
+			first_base_pitch = pitch
+		end
+
+		local new_item		= reaper.AddMediaItemToTrack(new_items_track)
+		local new_item_take = reaper.AddTakeToMediaItem(new_item)
+
+		reaper.SetMediaItemInfo_Value(new_item, "D_VOL", vel/127.0)
+		reaper.SetMediaItemTakeInfo_Value(new_item_take, "D_PITCH", pitch - first_base_pitch)
+		reaper.SetMediaItemTakeInfo_Value(new_item_take, "B_PPITCH", 1)
+
+		local start_pos	= reaper.MIDI_GetProjTimeFromPPQPos(midi_take, start_ppq_pos)
+		local end_pos	= reaper.MIDI_GetProjTimeFromPPQPos(midi_take, end_ppq_pos)
+
+		reaper.SetMediaItemPosition(new_item, start_pos, false)
+		reaper.SetMediaItemLength(new_item, end_pos-start_pos, false)
+
+		::continue::
+	end
+
+	reaper.Undo_EndBlock("Items Based on MIDI", 0)
 	reaper.UpdateArrange()
 end
 
