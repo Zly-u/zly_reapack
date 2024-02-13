7e0e28b68a08a5e9af8e625c0023c370f981eef6
Zly <zlyudchik@gmail.com>
2024-02-13 19:31:15 +0300

    Test Preview Image in the script About section.

--- MIDI/meta_ItemsBasedOnMIDI.lua
+++ MIDI/meta_ItemsBasedOnMIDI.lua
@@ -12,6 +12,12 @@
 	- Each of the item's pitch is based off the first note in the MIDI.
 	- Inherits Volume from midi.
 	- Creates Channels in their own Folder Tracks for ease of work.
+
+	# USAGE
+	1. Select MIDI (Right now works only with one selected MIDI at the time)
+	2. Use the script
+@screenshot
+	Preview https://github.com/Zly-u/NAGASHIZAR_reapack/blob/master/MIDI/ItemsBasedOnMIDI/img_MIDI2Items.png
 @changelog
 	- Renamed script to be more compact with naming
 	- Fixed weird case with Channel 0 not being sorted.
