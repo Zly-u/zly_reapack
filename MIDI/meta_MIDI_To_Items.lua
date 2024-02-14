5b1e15e53d23896b427152ad67569a45b9896a3c
Zly <zlyudchik@gmail.com>
2024-02-14 16:37:16 +0300

    - removed bugged versions

--- MIDI/meta_MIDI_To_Items.lua
+++ MIDI/meta_MIDI_To_Items.lua
@@ -17,5 +17,5 @@
 @screenshot
 	Image Preview https://github.com/Zly-u/NAGASHIZAR_reapack/blob/master/MIDI/MIDI_To_Items/img_MIDI2Items.png
 @changelog
-	- Added button for setting all Channels with the same source.
+	- Added buttons for clearing samples.
 --]]
