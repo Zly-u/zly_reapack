161982f0711a7f96650f8450ea17f345ae4809fd
Zly <zlyudchik@gmail.com>
2024-02-14 15:16:44 +0300

    - Used a right link for the image preview

--- MIDI/meta_MIDI_To_Items.lua
+++ MIDI/meta_MIDI_To_Items.lua
@@ -15,7 +15,7 @@
 	- Able to choose a sample source file for each Channel before generating all the Media Items.
 	- And many more.
 @screenshot
-	Image Preview https://github.com/Zly-u/NAGASHIZAR_reapack/blob/master/MIDI/ItemsBasedOnMIDI/img_MIDI2Items.png
+	Image Preview https://github.com/Zly-u/NAGASHIZAR_reapack/blob/master/MIDI/MIDI_To_Items/img_MIDI2Items.png
 @changelog
 	- Added GUI and many other new features to the script.
 --]]
